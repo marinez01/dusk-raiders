fcd28fd733e5507883d5774edc460605d2682f6b
marinez01 <martin.massen@hotmail.com>
2025-06-02 17:05:37 +0000

    improve error handling in deploy script

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -12,3 +12,4 @@ contract Collection {
     // 2025-05-01: refactor contract for gas efficiency
     // 2025-05-03: update license headers
     // 2025-05-12: clean up unused imports
+    // 2025-06-02: improve error handling in deploy script
