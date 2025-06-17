cd17741b4d5ff58b308b31bfc5571dc980a474ac
marinez01 <martin.massen@hotmail.com>
2025-06-17 18:48:35 +0000

    add initial project scaffold

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -16,3 +16,4 @@ contract Collection {
     // 2025-06-03: tweak gas limits in config
     // 2025-06-13: improve error handling in deploy script
     // 2025-06-16: add security notes
+    // 2025-06-17: add initial project scaffold
