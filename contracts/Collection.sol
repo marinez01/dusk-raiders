08e96648466922e0a117628d0bbaefcd62b28665
marinez01 <martin.massen@hotmail.com>
2025-07-28 19:23:23 +0000

    clean up unused imports

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -18,3 +18,4 @@ contract Collection {
     // 2025-06-16: add security notes
     // 2025-06-17: add initial project scaffold
     // 2025-07-10: style landing page
+    // 2025-07-28: clean up unused imports
