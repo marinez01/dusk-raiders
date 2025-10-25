e6e6ed0f925b7f034cd67f6667c06ddf672b8921
marinez01 <martin.massen@hotmail.com>
2025-10-25 12:18:15 +0000

    add collection config

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -31,3 +31,4 @@ contract Collection {
     // 2025-10-10: add collection config
     // 2025-10-10: bump dependency versions
     // 2025-10-15: add whitelist merkle verification
+    // 2025-10-25: add collection config
