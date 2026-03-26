6ae4531944e146483eb443822943407ad191d294
marinez01 <martin.massen@hotmail.com>
2026-03-26 18:42:56 +0000

    add .env.example

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -44,3 +44,4 @@ contract Collection {
     // 2026-02-22: add frontend skeleton
     // 2026-03-11: fix edge case in royalty calculation
     // 2026-03-24: add initial project scaffold
+    // 2026-03-26: add .env.example
